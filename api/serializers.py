398b58d1628021008658a7e29c24faf966df7fa0
avishek376 <avishekdeb376@gmail.com>
2024-03-01 19:29:31 +0530

    commit serializer

--- api/serializers.py
+++ api/serializers.py
@@ -8,6 +8,7 @@ class UserCreateSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data["password"] = make_password(validated_data["password"])  # hash password
         user = User.objects.create(**validated_data)  # create user
+        # DETAILS:: User profile will get created when an user instance will get created
         UserProfile.objects.create(user=user)  # create user profile by user instance
         return user
 
